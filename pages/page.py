e8f8f649b55ed066b111421fedc3971bf035d58b
Erik Marty <emarty@broadway.com>
2015-06-26 15:25:23 -0400

    Updated pages classes

--- pages/page.py
+++ pages/page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotVisibleException
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from unittestzero import Assert
 import time
 
@@ -38,6 +40,34 @@ class Page(object):
                          "Expected page title: %s. Actual page title: %s" % (self._page_title, self.page_title))
         return True
 
+    def is_element_visible(self, *locator):
+        try:
+            return self.selenium.find_element(*locator).is_displayed()
+        except (ElementNotVisibleException, NoSuchElementException):
+            # this will return a snapshot, which takes time.
+            return False
+
+    def is_element_present(self, *locator):
+        """
+        Return true if the element at the specified locator is present in the DOM.
+        Note: It returns false immediately if the element is not found.
+        """
+        self.selenium.implicitly_wait(0)
+        try:
+            self._selenium_root.find_element(*locator)
+            return True
+        except NoSuchElementException:
+            return False
+        finally:
+            # set the implicit wait back
+            self.selenium.implicitly_wait(self.testsetup.default_implicit_wait)
+
+    def wait_until_element_visible(self, element):
+        # overriding timeout here to higher value of 360
+        timeout = 30
+        self.selenium.execute_script("window.scrollTo(0, 0)")
+        WebDriverWait(self.selenium, timeout).until(EC.visibility_of(element))
+
     def wait_for_element_present(self, *locator):
         """Wait for the element at the specified locator to be present in the DOM."""
         count = 0
@@ -67,28 +97,17 @@ class Page(object):
         finally:
             self.selenium.implicitly_wait(self.testsetup.default_implicit_wait)
 
-    def force_wait(self):
-        try:
-            time.sleep(5)
-        except TimeoutException:
-            Assert.fail(TimeoutException)
-        finally:
-            self.selenium.implicitly_wait(self.testsetup.default_implicit_wait)
-
-    def is_element_present(self, *locator):
-        """
-        Return true if the element at the specified locator is present in the DOM.
-        Note: It returns false immediately if the element is not found.
-        """
-        self.selenium.implicitly_wait(0)
-        try:
-            self._selenium_root.find_element(*locator)
-            return True
-        except NoSuchElementException:
-            return False
-        finally:
-            # set the implicit wait back
-            self.selenium.implicitly_wait(self.testsetup.default_implicit_wait)
+    def select_option(self, value, locator):
+        dropdown = self.selenium.find_element(*locator)
+        option_found = False
+        all_options = dropdown.find_elements_by_tag_name("option")
+        for option in all_options:
+            if option.get_attribute("value") == value:
+                option_found = True
+                option.click()
+                break
+        if option_found is False:
+            raise Exception("Option '" + value + "' was not found, thus not selectable.")
 
     def get_url_current_page(self):
         return(self.selenium.current_url)
@@ -101,6 +120,13 @@ class Page(object):
         """Return a list of elements at the specified locator."""
         return self._selenium_root.find_elements(*locator)
 
+    def force_wait(self):
+        try:
+            time.sleep(5)
+        except TimeoutException:
+            Assert.fail(TimeoutException)
+        finally:
+            self.selenium.implicitly_wait(self.testsetup.default_implicit_wait)
 
 class PageRegion(Page):
     """Base class for a page region (generally an element in a list of elements)."""
